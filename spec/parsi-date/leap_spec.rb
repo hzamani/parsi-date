b124dd3cff12d9e9945c242fca6ac5b519225574
Hassan Zamani <hsn.zamani@gmail.com>
2022-11-30 18:22:42 +0330

    Fix leap years
    closes #13

--- spec/parsi-date/leap_spec.rb
+++ spec/parsi-date/leap_spec.rb
@@ -1,13 +1,15 @@
 describe "Parsi::Date#leap?" do
   it "returns true if a year is a leap year in the Parsi (Jalali) calendar" do
-    expect(Parsi::Date.leap?(1387)).to be_truthy
-    expect(Parsi::Date.leap?(1391)).to be_truthy
-    expect(Parsi::Date.leap?(1395)).to be_truthy
+    expect Parsi::Date.leap?(1387) == true
+    expect Parsi::Date.leap?(1391) == true
+    expect Parsi::Date.leap?(1395) == true
+    expect Parsi::Date.leap?(1403) == true
   end
 
   it "returns false if a year is not a leap year in the Parsi (Jalali) calendar" do
-    expect(Parsi::Date.leap?(1390)).to be_falsey
-    expect(Parsi::Date.leap?(1392)).to be_falsey
-    expect(Parsi::Date.leap?(1400)).to be_falsey
+    expect Parsi::Date.leap?(1390) == false
+    expect Parsi::Date.leap?(1392) == false
+    expect Parsi::Date.leap?(1400) == false
+    expect Parsi::Date.leap?(1404) == false
   end
 end
